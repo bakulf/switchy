7d5212e8af055da9903d9e1030fe15dd3599407d
Andrea Marchesini <baku@ippolita.net>
2011-08-29 09:52:59 +0200

    Maybe a better way to add the icon in the navBar

--- content/switchy-overlay.js
+++ content/switchy-overlay.js
@@ -3,13 +3,13 @@ var navBar    = document.getElementById("nav-bar");
 var curSet    = navBar.currentSet.split(",");
     
 if (curSet.indexOf(myId) == -1) {
-    var pos = curSet.length;
-    var set = curSet.slice(0, pos).concat(myId).concat(curSet.slice(pos));
-    
+    var set = curSet.slice(0, curSet.length).concat(myId).concat(curSet.slice(curSet.length));
+
     navBar.setAttribute("currentset", set.join(","));
     navBar.currentSet = set.join(",");
     document.persist(navBar.id, "currentset");
     try {
         BrowserToolboxCustomizeDone(true);
-    } catch (e) { }
+    }
+    catch (e) {}
 }
